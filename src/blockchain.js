7f67f99540bc8ada38dd5a96dc554a18a8494420
Jerome <654321su@gmail.com>
2019-01-01 20:04:15 +0900

    #09 Validating Chain

--- src/blockchain.js
+++ src/blockchain.js
@@ -57,7 +57,10 @@ const getBlocksHash = (block) => createHash(
 );
 
 const isNewBlockValid = (candidateBlock, latestBlock) => {
-  if (latestBlock.index + 1 === candidateBlock.index) {
+  if(!isNewStructureValid(candidateBlock)) {
+    console.log('The candidateBlock structure is not valid');
+    return false;
+  } else if (latestBlock.index + 1 === candidateBlock.index) {
     console.log('The candidate block does not have a valid index');
     return false;
   } else if (latestBlock.hash !== candidateBlock.previousHash) {
@@ -78,4 +81,20 @@ const isNewStructureValid = (block) => {
     && typeof block.timestamp === 'number'
     && typeof block.data === 'string'
   );
+};
+
+const isChainValid = (candidateChain) => {
+  const isGenesisValid = block => {
+    return JSON.stringify(block) === JSON.stringify(genesisBlock);
+  };
+  if (!isGenesisValid(candidateChain[0])) {
+    console.log('The candidateChain\'s genesisBlock is not the same as our genesisBlock');
+    return false;
+  }
+  for(let i = 1; i < candidateChain.length; i++) {
+    if (!isNewBlockValid(candidateChain[i], candidateChain[i - 1])) {
+      return false;
+    }
+  }
+  return true;
 };
