05ba8cde0c422fad3d3ba94bad9e8d5475db97e0
Jerome <654321su@gmail.com>
2019-01-01 19:15:12 +0900

    #04 Creating the Genesis Block

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,15 @@ class Block {
     this.data = data;
   }
 }
+
+const genesisBlock = new Block(
+  0,
+  '5A193CC5D9197585F4EEF9E2B8064E58AB3345372BDD5BE36E48CB9249D4333A',
+  null,
+  1546337562415,
+  'This is the genesis!!'
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain);
