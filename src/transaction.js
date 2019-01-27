dbe879f2911e217b57060f9244cd69fc1e763cf5
Jerome <654321su@gmail.com>
2019-01-27 19:44:20 +0900

    #40 Updating uTxOuts part Two

--- src/transaction.js
+++ src/transaction.js
@@ -72,4 +72,9 @@ const updateUTxOuts = (newTxs, uTxOutList) => {
     });
   })
   .reduce((a, b) => a.concat(b), []);
+
+  const spentTxOuts = newTxs
+    .map(tx => tx.txIns)
+    .reduce((a, b) => a.concat(b), [])
+    .map(txIn => new UTxOut(txIn.txOutId, txIn.txOutIndex, '', 0));
 };
