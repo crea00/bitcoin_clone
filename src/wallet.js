aaa74be68debad9351ce490c5aa35330c9de32e5
Jerome <654321su@gmail.com>
2019-02-05 23:05:24 +0900

    Add the missing part of previous commit
     - Add a dependency: lodash
     - Create a function: getBalance

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,7 @@
 const EC = require('elliptic').ec,
   path = require('path'),
-  fs = require('fs');
+  fs = require('fs'),
+  _ = require('lodash');
 
 const ec = new EC('secp256k1');
 
@@ -23,6 +24,13 @@ const getPublicFromWallet = () => {
   const key = ec.keyFromPrivate(privateKey, 'hex');
 
   return key.getPublic().encode('hex');
+};
+
+const getBalance = (address, uTxOuts) => {
+  return _(uTxOuts)
+    .filter(uTxO => uTxO.address === address)
+    .map(uTxO => uTxO.amount)
+    .sum();
 };
 
 const initWallet = () => {
